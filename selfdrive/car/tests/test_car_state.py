16506c48c964f59adc4032a62a23324cb1014cb7
Andrew Chan <email@suspi.net>
2020-03-01 14:47:05 -0800

    Update test_car_state.py

--- selfdrive/car/tests/test_car_state.py
+++ selfdrive/car/tests/test_car_state.py
@@ -4,19 +4,41 @@ import cereal.messaging as messaging
 
 def start_check():
   sm = messaging.SubMaster(['carState'])
+  sm.update()
 
-  print("open a door")
+#  check_signal(sm, sm['carState'].canValid, "Can Valid") #@26 :Bool;
+  check_signal(sm, sm['carState'].doorOpen, "Door Open")
+  check_signal(sm, sm['carState'].seatbeltUnlatched, "Seatbelt Unlatched")
+  check_signal(sm, sm['carState'].buttonEvents, "Button Events")
+  check_signal(sm, sm['carState'].leftBlinker, "Left Blinker") #@20 :Bool;
+  check_signal(sm, sm['carState'].rightBlinker, "Right Blinker") #@21 :Bool;
+  check_signal(sm, sm['carState'].genericToggle, "Generic Toggle") #@23 :Bool;
 
-  while True:
+
+def check_signal(sm, signal, desc):
+  lastStatus = signal
+  count = 0
+
+  print("Test ", desc)
+  print("Current value ", lastStatus)
+
+  checking = True
+  start = time.time()
+
+  while checking:
     sm.update()
 
-    # Get interaction
     if sm.updated['carState']:
-
-      isDoorOpen = sm['carState'].doorOpen
-
-      if isDoorOpen:
-          print("door is open")
+      signal = sm['carState'].doorOpen
+      if lastStatus != signal:
+         lastStatus = signal
+         count += 1
+         print(desc, " changed ", lastStatus, count)
+    if time.time() > start + 10:
+      #check with user if they want to exit
+      checking = False
+    if count > 10:
+      checking = False
 
 if __name__ == "__main__":
   start_check()
